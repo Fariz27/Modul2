7b20f6e3d670422586f550697dd194bd3fae1ea7
Fariz <Fariz@LAPTOP-JQ2AQ0I3>
2018-09-20 07:10:46 +0700

    Final

--- src/TugasPraktikum/TugasPraktikum2.java
+++ src/TugasPraktikum/TugasPraktikum2.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package TugasPraktikum;
-
+import java.util.Scanner;
 /**
  *
  * @author Fariz
  */
 public class TugasPraktikum2 {
+    private double jariJari,Diameter,LuasPermukaan,Volume;
+     Scanner s = new Scanner(System.in);
+    public void setJariJari() {
+        this.jariJari = s.nextDouble();
+    }
+    public double showJariJari() {
+        return jariJari;
+    }
+
+    public double showDiameter() {
+        this.Diameter = jariJari*2;
+        return Diameter;
+    }
+
+    public double showLuasPermukaan() {
+        this.LuasPermukaan=4*Math.PI*jariJari*jariJari;
+        return LuasPermukaan;
+    }
+
+    public double showVolume() {
+        this.Volume=4/3*Math.PI*jariJari*jariJari*jariJari;
+        return Volume;
+    }
     
 }
--- src/TugasPraktikum/TugasPraktikumm2Uji.java
+++ src/TugasPraktikum/TugasPraktikumm2Uji.java
@@ -10,5 +10,12 @@ package TugasPraktikum;
  * @author Fariz
  */
 public class TugasPraktikumm2Uji {
-    
+    public static void main(String[] args){
+        TugasPraktikum2 b = new TugasPraktikum2();
+        System.out.print("Masukan Jari-Jari Bola\t: ");
+        b.setJariJari();
+        System.out.println("Diameter Bola\t\t: "+b.showDiameter());
+        System.out.println("Luas Permukaan Bola\t: "+b.showLuasPermukaan());
+        System.out.println("Volume Bola\t\t: "+b.showVolume());
+    }
 }
